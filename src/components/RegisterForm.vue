a746b1eab8a1b280b8859b6f86b110be40df74fb
dreadnought2099 <raymart.magallanes@bisu.edu.ph>
2024-11-15 09:46:52 +0800

    added local storage for input

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -4,23 +4,25 @@
     <form @submit.prevent="register">
       <div>
         <label for="name">Name</label>
-        <input type="text" v-model="user.name" required />
+        <input id="name" type="text" v-model="user.name" required />
       </div>
       <div>
         <label for="email">Email</label>
-        <input type="email" v-model="user.email" required />
+        <input id="email" type="email" v-model="user.email" required />
       </div>
       <div>
         <label for="password">Password</label>
-        <input type="password" v-model="user.password" required />
+        <input id="password" type="password" v-model="user.password" required />
       </div>
-      <button type="submit">Register</button>
+      <div>
+        <button type="submit">Register</button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted, watch, onBeforeUnmount } from "vue";
 import "./css/RegisterForm.css";
 
 export default {
@@ -31,18 +33,45 @@ export default {
       password: "",
     });
 
+    const users = ref([]);
+
+    onMounted(() => {
+      users.value = JSON.parse(localStorage.getItem("users")) || [];
+      const persistedUser = JSON.parse(localStorage.getItem("currentUser"));
+      if (persistedUser) {
+        user.value = persistedUser;
+      }
+    });
+
+    watch(user, (newUser) => {
+      localStorage.setItem("currentUser", JSON.stringify(newUser));
+    });
+
+    const saveUserBeforeUnload = () => {
+      localStorage.setItem("currentUser", JSON.stringify(user.value));
+    };
+
+    window.addEventListener("beforeunload", saveUserBeforeUnload);
+
+    onBeforeUnmount(() => {
+      window.removeEventListener("beforeunload", saveUserBeforeUnload);
+    });
+
     const register = () => {
-      const users = JSON.parse(localStorage.getItem("users")) || [];
-      users.push({ ...user.value });
-      localStorage.setItem("users", JSON.stringify(users));
+      users.value.push({ ...user.value });
+      localStorage.setItem("users", JSON.stringify(users.value));
 
       alert("Registration successful!");
+
       user.value = { name: "", email: "", password: "" };
+
+      localStorage.removeItem("currentUser");
     };
 
     return {
       user,
       register,
+      users,
     };
   },
 };
